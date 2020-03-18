032748c7d159eef5da873a204e41c3b9f71c70b3
Valentyn Khoroshchak <smartgtsua@gmail.com>
2020-03-18 17:48:01 +0200

    Add class-based namespace and change coding style to OOP

--- client.py
+++ client.py
@@ -3,31 +3,46 @@ import socketio
 sio = socketio.Client()
 
 
-@sio.event
-def connect():
-    print("connected")
+class CoffeeMachineNamespace(socketio.ClientNamespace):
+    def on_connect(self):
+        print("connected")
+
+    def on_disconnect(self):
+        print("disconnected")
+
+    def on_my_response(self, data):
+        print(data)
 
 
-@sio.event
-def disconnect():
-    print("disconnected")
+class CoffeeMachineManager:
+    namespace = '/coffee'
 
+    @staticmethod
+    def make_coffee(data):
+        sio.emit("make_coffee", data, namespace=CoffeeMachineManager.namespace)
 
-@sio.event
-def my_response(data):
-    print(data)
+    @staticmethod
+    def show_orders():
+        sio.emit("show_orders", namespace=CoffeeMachineManager.namespace)
+
+    @staticmethod
+    def show_goods():
+        sio.emit("show_goods", namespace=CoffeeMachineManager.namespace)
 
 
 if __name__ == "__main__":
-    sio.connect("http://localhost:5000")
-    sio.emit("show_goods")
-    sio.emit("make_coffee", {"drink": "Latte", "add": "sugar"})
-    sio.emit("show_goods")
-    sio.emit("make_coffee", {"drink": "Latte"})
-    sio.emit("show_goods")
-    sio.emit("make_coffee", {"drink": "Latte", "add": "ABRA"})
-    sio.emit("show_goods")
-    sio.emit("make_coffee", {"drink": "ABRA", "add": "sugar"})
-    sio.emit("show_goods")
-    sio.emit("show_orders")
-    sio.wait()
+    sio.register_namespace(CoffeeMachineNamespace('/coffee'))
+    sio.connect("http://localhost:5000", namespaces=['/coffee'])
+
+    orders = [{"drink": "Latte", "add": "sugar"}, {"drink": "Latte"}, {"drink": "Latte", "add": "ABRA"},
+              {"drink": "ABRA", "add": "sugar"}]
+
+    CoffeeMachineManager.show_goods()
+
+    for order in orders:
+        CoffeeMachineManager.make_coffee(order)
+        CoffeeMachineManager.show_goods()
+
+    CoffeeMachineManager.show_orders()
+
+
--- server.py
+++ server.py
@@ -6,81 +6,85 @@ sio = socketio.Server(async_mode='eventlet')
 
 app = socketio.WSGIApp(sio)
 
-goods = {
-    "drinks": {
-        "Americano": 20,
-        "Latte": 20,
-        "Cappuccino": 15,
-        "Espresso": 20,
-        "Irish Coffee": 7,
-    },
-    "adds": {
-        "milk": 8,
-        "sugar": 8,
-        "whiskey": 7
+
+class CoffeeMachine:
+    goods = {
+        "drinks": {
+            "Americano": 20,
+            "Latte": 20,
+            "Cappuccino": 15,
+            "Espresso": 20,
+            "Irish Coffee": 7,
+        },
+        "adds": {
+            "milk": 8,
+            "sugar": 8,
+            "whiskey": 7
+        }
     }
-}
+    orders = {}
 
-orders = {}
+    @staticmethod
+    def make_order(sid, data):
+        order = {"time": datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'), "order description": data}
 
+        if sid not in CoffeeMachine.orders:
+            CoffeeMachine.orders[sid] = [order]
+        else:
+            CoffeeMachine.orders[sid].append(order)
 
-@sio.event
-def connect(sid, environ):
-    print("Connect ", sid)
-    sio.emit("my_response", "Hi from coffee machine! :)")
+    @staticmethod
+    def make_coffee(sid, data):
+        response = CoffeeMachine.validate(data)
 
+        if "OK" in response:
+            if 'add' in data:
+                CoffeeMachine.goods['adds'][data['add']] -= 1
 
-@sio.event
-def disconnect(sid):
-    print('Disconnect ', sid)
+            CoffeeMachine.goods['drinks'][data['drink']] -= 1
+            CoffeeMachine.make_order(sid, data)
 
+        return response
 
-@sio.event
-def show_goods(sid):
-    sio.emit('my_response', {"OK": 200, "Goods": goods})
-
-
-@sio.event
-def show_orders(sid):
-    sio.emit('my_response', {"OK": 200, "Orders": orders})
-
-
-@sio.event
-def make_coffee(sid, data):
-    response = validate(data)
-    if "OK" in response:
-        if 'add' in data:
-            goods['adds'][data['add']] -= 1
-        goods['drinks'][data['drink']] -= 1
-        make_order(sid, data)
-    sio.emit('my_response', response)
-
-
-def make_order(sid, data):
-    order = {"time": datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'), "order description": data}
-
-    if sid not in orders:
-        orders[sid] = [order]
-    else:
-        orders[sid].append(order)
-
-
-def validate(data):
-    if 'drink' not in data:
-        return {"Error": 400, "Description": "Bad request. You didn't mention 'drink' in your request"}
-    elif data['drink'] not in goods['drinks']:
-        return {"Error": 404, "Description": "Not found. You want to order unknown 'drink'"}
-    elif goods['drinks'][data['drink']] == 0:
-        return {"Error": 403, "Description": "Forbidden. Out of stock!"}
-
-    elif 'add' in data:
-        if data['add'] not in goods['adds']:
-            return {"Error": 404, "Description": "Not found. You want to order unknown 'add'"}
-        elif goods['adds'][data['add']] == 0:
+    @staticmethod
+    def validate(data):
+        if 'drink' not in data:
+            return {"Error": 400, "Description": "Bad request. You didn't mention 'drink' in your request"}
+        elif data['drink'] not in CoffeeMachine.goods['drinks']:
+            return {"Error": 404, "Description": "Not found. You want to order unknown 'drink'"}
+        elif CoffeeMachine.goods['drinks'][data['drink']] == 0:
             return {"Error": 403, "Description": "Forbidden. Out of stock!"}
 
-    return {"OK": 201, "Description": "Coffee created!"}
+        elif 'add' in data:
+            if data['add'] not in CoffeeMachine.goods['adds']:
+                return {"Error": 404, "Description": "Not found. You want to order unknown 'add'"}
+            elif CoffeeMachine.goods['adds'][data['add']] == 0:
+                return {"Error": 403, "Description": "Forbidden. Out of stock!"}
+
+            return {"OK": 201, "Description": f"{data['drink']} with {data['add']} is created!"}
+
+        return {"OK": 201, "Description": f"{data['drink']} is created!"}
+
+
+class CoffeeMachineNamespace(socketio.Namespace):
+    namespace = '/coffee'
+    def on_connect(self, sid, environ):
+        print("Connect ", sid)
+        sio.emit("my_response", "Hi from coffee machine! :)", namespace=self.namespace)
+
+    def on_disconnect(self, sid):
+        print('Disconnect ', sid)
+
+    def on_show_goods(self, sid):
+        sio.emit('my_response', {"OK": 200, "Goods": CoffeeMachine.goods}, namespace=self.namespace)
+
+    def on_show_orders(self, sid):
+        sio.emit('my_response', {"OK": 200, "Orders": CoffeeMachine.orders}, namespace=self.namespace)
+
+    def on_make_coffee(self, sid, data):
+        sio.emit('my_response', CoffeeMachine.make_coffee(sid, data), namespace=self.namespace)
 
 
 if __name__ == '__main__':
+    sio.register_namespace(CoffeeMachineNamespace('/coffee'))
     eventlet.wsgi.server(eventlet.listen(('localhost', 5000)), app)
