05cc6b0689955ef4fe917740a099d57a9943eb0f
Valentyn Khoroshchak <smartgtsua@gmail.com>
2020-03-18 12:38:02 +0200

    Add validate() method to order

--- server.py
+++ server.py
@@ -37,5 +37,16 @@ def show_goods(sid):
     sio.emit('my_response', {"OK": 200, "Goods": goods})
 
 
+def validate(data):
+    if 'drink' not in data:
+        return {"Error": 400, "Description": "Bad request. You didn't mention 'drink' in your request"}
+    elif data['drink'] not in goods['drinks']:
+        return {"Error": 404, "Description": "Not found. You want to order unknown 'drink'"}
+    elif goods['drinks'][data['drink']] == 0:
+        return {"Error": 403, "Description": "Forbidden. Out of stock!"}
+    else:
+        return {"OK": 201, "Description": "Coffee created!"}
+
+
 if __name__ == '__main__':
     eventlet.wsgi.server(eventlet.listen(('localhost', 5000)), app)
